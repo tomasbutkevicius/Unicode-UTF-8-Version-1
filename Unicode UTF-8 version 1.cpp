c219d9e99942604e2add4a8fce9edf4e8bc70961
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2019-10-07 15:05:05 +0300

    gets utf8 but only in bin; prints some symbols in visual std

--- Unicode UTF-8 version 1.cpp
+++ Unicode UTF-8 version 1.cpp
@@ -5,18 +5,27 @@
 #include <vector>
 #include <string>
 #include <bitset>
+#include <fcntl.h>
+#include <io.h>
+#include <stdio.h>
+#include <clocale>
+#define UNICODE
+#define _UNICODE
 using namespace std;
 class converter {
 private:
 	int dec;
+	int decimal;
 	string binary;
 	string hex;
+	string utf8;
 public:
-	converter() :dec(0),binary(""), hex("") {};
+	converter() :dec(0), decimal(0),binary(""), hex(""), utf8("") {};
 
 	void input() {
 		cout << "Enter decimal number: ";
 		cin >> dec;
+		decimal = dec;
 		cout << endl;
 	}
 
@@ -88,17 +97,105 @@ public:
 		}
 		return hex;
 	}
+
+	void getUtf() 
+	{
+		int x_vietu(0);
+		int pildyt_nuliais(0);
+		int pildyt_bin=0;
+		cout << endl;
+		vector <string> bytes = {};
+		if (decimal <= 127) //Byte 1
+		{
+			bytes.push_back("0xxxxxxx");
+			x_vietu = 7;
+			pildyt_nuliais = x_vietu - binary.length();
+		}
+		else if (decimal <= 2047) //Byte 2
+		{
+			bytes.push_back("110xxxxx");
+			bytes.push_back("10xxxxxx");
+			x_vietu = 16-5;
+			pildyt_nuliais = x_vietu - binary.length();
+			//uzpildo nuliais vietas iki binary
+			
+			
+		}
+		else if (decimal <= 65535) //Byte 3
+		{
+			//1110xxxx 10xxxxxx 10xxxxxx
+			bytes.push_back("1110xxxx");
+			bytes.push_back("10xxxxxx");
+			bytes.push_back("10xxxxxx");
+			x_vietu = 8 * 3 - 8;
+			pildyt_nuliais = x_vietu - binary.length();
+			
+		}
+		else if (decimal <= 1114111) //Byte 4
+		{
+			//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			bytes.push_back("11110xxx");
+			bytes.push_back("10xxxxxx");
+			bytes.push_back("10xxxxxx");
+			bytes.push_back("10xxxxxx");
+			x_vietu = 8 * 4 - 11;
+			pildyt_nuliais = x_vietu - binary.length();
+
+		}
+		for (int i = 0; i < bytes.size(); i++)
+			{
+				if (pildyt_nuliais > 0)
+				{
+					for (int j = 0; j < bytes[i].length(); j++)
+					{
+						if (bytes[i][j] == 'x'&&pildyt_nuliais>0)
+						{
+						bytes[i][j] = '0'; 
+						pildyt_nuliais--;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < bytes.size(); i++)
+			{
+				for (int j = 0; j < bytes[i].length(); j++)
+				{
+					if (bytes[i][j] == 'x')
+					{	
+						bytes[i][j] = binary[pildyt_bin];
+						pildyt_bin++;
+					}
+				}
+			}
+		for(int i=0;i<bytes.size();i++)
+		cout << bytes[i]<< " ";
+	}
+
 	~converter() {};
 };
-
-
-
 int main()
 {
+	setlocale(LC_ALL, "");
+	//http://cppwhispers.blogspot.com/2012/11/unicode-and-your-application-3-of-n.html
 	converter test;
+	unsigned int hex;
 	test.input();
 	test.decToBinary();
-	cout << "HEX: "<<test.getHex();
 
-	//vector<string> bytes = {"00000000", "00000000", "00000000", "00000000"};
+	cout << "HEX: "<<test.getHex();
+	constexpr unsigned char s[4] = { '\u00C4', '\u0920', '\u047C', '\u00D2' };
+	cout << endl;
+	for(int i=0;i<4;i++)
+	cout<<s[i]<<" ";
+	test.getUtf();
+
 }
+//Get dec from hex:
+		//number = stoi(test.getHex(), 0, 16);	
+	
+	/*
+	klausimai: 
+	Ar nieko daryti sita su java, ar nemaisys, kai kita uzd padaryta su c++
+	Ar ir turi buti tik vienas rodomas simbolis is situ..?
+	reiks paskui paverst i skaiciu, is int gales paverst i zenkla
+	*/
